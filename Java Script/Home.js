b9b2310fb6af84843d2d1e6bc82c2a6fa9a22742
KaveeshJeffry <kaveeshgg@gmail.com>
2024-03-15 19:03:18 +0530

    first commit

---
+++ Java Script/Home.js
@@ -0,0 +1,21 @@
+function about() {
+    const element = document.getElementById("skills");
+    element.scrollIntoView();
+}
+
+function projects() {
+    const element = document.getElementById("projects");
+  element.scrollIntoView();
+}
+
+function contact() {
+  const element = document.getElementById("contact");
+element.scrollIntoView();
+}
+
+var mybutton = document.getElementById("myBtn");
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
