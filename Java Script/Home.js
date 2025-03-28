e7411e475920a58d24c59c1e7553add56d5a90b9
Kaveesh Jeffry Vershan <130240364+KaveeshJeffry@users.noreply.github.com>
2025-03-28 17:07:49 +0530

    Update Home.js

--- Java Script/Home.js
+++ Java Script/Home.js
@@ -1,21 +1,26 @@
+
 function about() {
-    const element = document.getElementById("skills");
-    element.scrollIntoView();
+  const element = document.getElementById("skills");
+  element.scrollIntoView({ behavior: "smooth" });
 }
 
 function projects() {
-    const element = document.getElementById("projects");
-  element.scrollIntoView();
+  const element = document.getElementById("projects");
+  element.scrollIntoView({ behavior: "smooth" });
+}
+
+function experience() {
+  const element = document.getElementById("experience");
+  element.scrollIntoView({ behavior: "smooth" });
 }
 
 function contact() {
   const element = document.getElementById("contact");
-element.scrollIntoView();
+  element.scrollIntoView({ behavior: "smooth" });
 }
 
-var mybutton = document.getElementById("myBtn");
 
 function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
 }
